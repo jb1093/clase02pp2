f905df472ba575f498da091a357ee1b699acb017
corderomarcos2017 <corderomarcos2017@gmail.com>
2021-04-23 20:30:39 -0300

    subi ejercicio 6

---
+++ app/ejercicio6.php
@@ -0,0 +1,25 @@
+<?php
+	$opcion=rand(1,3);
+	switch ($opcion) {
+		case 1:
+			echo "Eligio Bariloche, hace frio";
+			break;
+		case 2:
+			echo "Eligio Ushuaia, hace frio";
+			break;
+		
+		default:
+			echo "Eligio Cataratas, hace calor";
+			break;
+	}
+
+
+
+
+
+
+
+
+
+
+?>
