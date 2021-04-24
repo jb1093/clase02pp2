fe1fe8e002bb5dceab62d96d8b11c1d109a99af5
corderomarcos2017 <corderomarcos2017@gmail.com>
2021-04-23 21:07:35 -0300

    ejercicio7

---
+++ app/ejercicio7.php
@@ -0,0 +1,29 @@
+<?php
+	$num1=rand(1,100);
+	$num2=rand(1,100);
+	$num3=rand(1,100);
+
+	echo "Numeros seleccionados: ".$num1. ";" .$num2. ";". $num3 . "<br>";
+
+	if($num1!=$num2 && $num2!=$num3 && $num1!=$num3){
+
+	}else{
+
+	}if($num1<$num2 && $num2<$num3){
+		echo "medio= ". $num2;
+	} else{
+		if($num1<$num3){
+			echo "medio= ". $num1;
+		} else{
+			echo "medio=" . $num3;
+		} 
+	} else {
+		echo "No hay medio";
+	}
+	
+
+
+
+
+
+?>
